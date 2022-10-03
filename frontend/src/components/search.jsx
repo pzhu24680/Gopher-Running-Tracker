9e759f85fbbc4cd3aa4988859fe749152c85ae67
Preston Zhu <pzhu24680@gmail.com>
2022-10-02 21:33:40 -0500

    add search feature

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,50 +1,67 @@
 import React, { useState } from "react";
 const Search = ({ id, onClick }) => {
-  const [value, setValue] = useState("");
-  const [inList, setInList] = useState(false);
-
-  const FoundEntry = <div />;
-
-  const checkEnter = (k) => {
-    if (k === "Enter") {
-      setValue("");
-      findEntry();
-    }
-  };
-
-  const findEntry = () => {
-    id.map((entry) => {
-      if (entry.date === value) {
-        return (
-          <div className="dayEntry">
-            <div className="miles-ran">Miles Ran: {entry.miles}</div>
-            <div className="average-pace">Pace: {entry.avgPace}</div>
-            <div className="time">Date: {entry.date}</div>
-            <div className="additional-notes">
-              Additional Notes: {entry.additionalNotes}
-            </div>
-            <button onClick={setInList(false)}>Remove Search</button>
-          </div>
-        );
-      }
-    });
-  };
-
-  return (
-    <div className="search-bar-holder">
-      <input
-        type="text"
-        className="search-bar"
-        placeholder="search date"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-        onKeyPress={checkEnter()}
-      ></input>
-      <button className="search" onClick={findEntry()}>
-        Search
-      </button>
-    </div>
-  );
+ const [value, setValue] = useState("");
+ const [search, setSearch] = useState(false);
+ const [entry, setEntry] = useState({});
+ 
+ const FindEntry = () => {
+   if (search == true) {
+     let foundEntry = {};
+     let found = false;
+     id.map((entry) => {
+       console.log(entry.date + ", " + value);
+       if (entry.date === value) {
+         foundEntry = entry;
+         found = true;
+       }
+     });
+     if (found) {
+       return (
+         <div className="searchedEntry">
+           <div className="miles-ran">Miles Ran: {foundEntry.miles}</div>
+           <div className="average-pace">Pace: {foundEntry.avgPace}</div>
+           <div className="time">Date: {foundEntry.date}</div>
+           <div className="additional-notes">
+             Additional Notes: {foundEntry.additionalNotes}
+           </div>
+           <div>
+             <button className="exitSearch" onClick={() => setSearch(!search)}>
+               Remove Search
+             </button>
+           </div>
+         </div>
+       );
+     } else {
+       return (
+         <div>
+           <div>Not Found</div>
+           <button onClick={() => setSearch(!search)}>Return to Search</button>
+         </div>
+       );
+     }
+   }
+ };
+ 
+ return search ? (
+   FindEntry()
+ ) : (
+   <div className="search-bar-holder">
+     <input
+       type="text"
+       className="search-bar"
+       placeholder="search date"
+       onChange={(e) => setValue(e.target.value)}
+       value={value}
+     ></input>
+     <button className="search" onClick={() => setSearch(!search)}>
+       Search
+     </button>
+   </div>
+ );
 };
-
+ 
 export default Search;
+ 
+ 
+ 
+
